f5fc15ce0bd3310be338846e891fbfb528ebfbea
Pramit Sahoo <pramitsahoo.gnipst@gmail.com>
2022-02-02 08:58:45 +0530

    Add files via upload

---
+++ 01.C++ Basics/Basic-part1.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	// cout <<"Hello "<<" "<< "My name is Pramit";
+	
+	// variables
+	// int a = 2;
+	// int b = 3;
+	// int sum = a + b;
+	// cout<< sum << endl;
+	
+	// data types - char, int, float,double, bool
+	// char c = 'a';
+	// int a = 3;
+	// double b = 3.5;
+	// a = 4.5;
+	// bool d = true;
+	
+	// cout<< c << " "<< a <<" "<< b <<" " << d << endl;
+	
+	// long int ,long long int(for storing long integers)
+	
+	// long long int a = 12345;
+	
+	// operators
+	// cout << 2*3 <<endl;
+	// cout << 20 % 7 << endl;
+	
+	
+	// ++ a and a ++
+	// int a = 5;
+	// a = a + 1;
+	// cout << a << endl;
+	// cout<< a++ <<endl;
+	// cout<<++a <<endl;
+	 
+	// cout<< a-- <<endl;
+	// cout<< --a << endl;
+	
+	
+	
+	// ASCII 
+	// char c = 'a';
+	// char b = 'c';
+	// cout << (int) c << endl;
+	// cout << b - c << endl;
+	
+	// taking input
+	
+	// int a ;
+	// double b;
+	// char c ;
+	// cin >> a >> b >> c;
+	// cout << a << " "<< b << " "<< c << " "<< (int)c << '\n';
+	
+	// cin terminates when it gets either a space or \n
+	
+	int a , b ,c;
+	char d ;
+	cin >> d >> a;
+	cout<< d << " " << a;
+	// char takes only the first character so it will only take 1 as an input 
+	return 0;
+}
