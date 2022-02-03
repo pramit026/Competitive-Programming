cef31f1f2eec97dc68f38fb7eee1976fc1dbc886
Pramit Sahoo <pramitsahoo.gnipst@gmail.com>
2022-02-03 12:05:09 +0530

    Add files via upload

---
+++ 01.C++ Basics/basics-part3.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 0;
+
+int main(){
+	// int a = 2;
+	// int b  =3;
+	// int c = 1;
+	// cout << (b > a) || (b > c);
+	
+	// // &&  ||
+	
+	
+	// if else
+	
+	// int n ;
+	// cin >> n;
+	// if (n % 2 == 0){
+	// 	cout << "Yes" ;
+	// }
+	// else if(n % 3 == 0){
+	// 	cout<< "Divisible by 3";
+	// }
+	// else{
+	// 	cout<< "No" ;
+	// }
+	
+	
+	// loop
+	// while loop
+	// int n ;
+	// cin >> n;
+	// int i = 1;
+	// while(i <= n){
+	// 	cout<< i << endl;
+	// 	i ++ ;
+	// }
+	
+	// for loop
+	// for (int i = 1; i <= n; ++i)
+	// {
+	// 	cout<< i << endl;
+	// }
+			// pattern print
+	// for (int i = 1; i <= n; ++i)
+	// {
+	// 	for (int j = 1; j <= i; ++j)
+	// 	{
+	// 		cout << "*" ;
+	// 	}
+	// 	cout << endl;
+	// }
+	
+				// test cases generation
+	
+	// int t ;
+	// cin >> t;
+	// while(t--){
+		
+	// int n ;
+	// cin >> n;
+	
+	// for (int i = 1; i <= n; ++i)
+	// {
+	// 	for (int j = 1; j <= i; ++j)
+	// 	{
+	// 		cout << "*" ;
+	// 	}
+	// 	cout << endl;
+	// }
+	
+		// infinite loop
+	// while(1){
+		
+	// }
+	
+	
+		// jump statements - break and continue
+	
+	for (int i = 0; i < 10; ++i)
+	{
+		if(i == 8){
+			// break;
+			continue;
+		}
+		cout << i << endl;
+	}
+	
+	
+	return 0;
+	}
+// }
